c8380460221c26466896f157a4501ce97f42a77c
teletha <teletha@users.noreply.github.com>
2025-04-28 08:49:48 +0000

    deploy: 1b2e73d55f34a82da70af0444d30add1d87591a8

---
+++ root.js
@@ -0,0 +1,280 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"evergarden",
+		"evergarden.design",
+		"evergarden.host",
+		"evergarden.javadoc",
+		"evergarden.page",
+		"evergarden.web"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "APIPage",
+			"packageName": "evergarden.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ActivityPage",
+			"packageName": "evergarden.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll",
+			"packageName": "evergarden",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableAll",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableOutput",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableProduct",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableProject",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableSources",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableVersion",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.ÅssignableÅrbitrary",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDoll.Ìnstantiator",
+			"packageName": "evergarden",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDollModel",
+			"packageName": "evergarden",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "AutoMemoriesDollModel.Internal",
+			"packageName": "evergarden",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ClassInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "CodeHighlight",
+			"packageName": "evergarden.web",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Document",
+			"packageName": "evergarden",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DocumentInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "DocumentPage",
+			"packageName": "evergarden.page",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "EvergardenDSL",
+			"packageName": "evergarden.design",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "EvergardenDesignScheme",
+			"packageName": "evergarden.design",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ExecutableInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "FieldInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "HTML",
+			"packageName": "evergarden.web",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Hosting",
+			"packageName": "evergarden.host",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Index",
+			"packageName": "evergarden",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Index.Doc",
+			"packageName": "evergarden",
+			"type": "Record"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MemberInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MethodInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Page",
+			"packageName": "evergarden.page",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "ParameterizableInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Region",
+			"packageName": "evergarden",
+			"type": "Record"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "SampleInfo",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "SiteBuilder",
+			"packageName": "evergarden",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "SourceCode",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Styles",
+			"packageName": "evergarden.design",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TemplateStore",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TypeResolver",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Util",
+			"packageName": "evergarden.javadoc",
+			"type": "Class"
+		}
+	]
+}
